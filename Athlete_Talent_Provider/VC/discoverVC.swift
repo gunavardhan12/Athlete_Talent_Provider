c1e9a55b9f48e6fd61eb384bf5381df2b0a4bb3a
Raj Tiwari <tiwariraj1620@gmail.com>
2024-05-21 15:38:30 +0530

    implemented the tab view and matched the colours and aded profile image view

---
+++ Athlete_Talent_Provider/VC/discoverVC.swift
@@ -0,0 +1,20 @@
+//
+//  discoverVC.swift
+//  Athlete_Talent_Provider
+//
+//  Created by RaJ TiWaRi on 21/05/24.
+//
+
+import UIKit
+
+class discoverVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Athlete_Talent_Provider/VC/mainVC.swift
@@ -0,0 +1,21 @@
+//
+//  mainVC.swift
+//  Athlete_Talent_Provider
+//
+//  Created by RaJ TiWaRi on 21/05/24.
+//
+
+import UIKit
+
+class mainVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.tabBar.backgroundColor = .darkGray
+//        self.tabBar.tintColor = .red
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+}
