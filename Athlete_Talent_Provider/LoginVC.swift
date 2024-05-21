3b058664fb8aec3678f11e3ddb8a2eef7fcb38f6
gunavardhan12 <kothagattugunavardhan@gmail.com>
2024-05-21 12:18:57 +0530

    added login page

---
+++ Athlete_Talent_Provider/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  Athlete_Talent_Provider
+//
+//  Created by Wegile-Gunavardhan on 21/05/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
